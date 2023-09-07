df45b7ff6e2a0c8519b9b21076a22dcd6ea1fa90
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-09-07 15:39:58 +0500

    Task2

---
+++ Tyuiu.NosovaVD.Sprint0.Task2.V0.Lib/Tyuiu.NosovaVD.Sprint0.Task2.V0.Lib.cpp
@@ -0,0 +1,14 @@
+﻿// Tyuiu.NosovaVD.Sprint0.Task2.V0.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+class Service :public ISprint0Task2V0
+{
+	virtual int Add(int a, int b) override
+	{
+		return a + b; 
+	}
+};
---
+++ Tyuiu.NosovaVD.Sprint0.Task2.V0.Test/Tyuiu.NosovaVD.Sprint0.Task2.V0.Test.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.NosovaVD.Sprint0.Task2.V0.Lib/Tyuiu.NosovaVD.Sprint0.Task2.V0.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task2V0* service = new Service();
+			int a = 12;
+			int b = 15;
+			int c = service->Add(a, b);
+			Assert::AreEqual(27, c);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			ISprint0Task2V0* service = new Service();
+			int a = 10;
+			int b = 5;
+			int c = service->Add(a, b);
+			Assert::AreEqual(15, c);
+		}
+	};
+}
