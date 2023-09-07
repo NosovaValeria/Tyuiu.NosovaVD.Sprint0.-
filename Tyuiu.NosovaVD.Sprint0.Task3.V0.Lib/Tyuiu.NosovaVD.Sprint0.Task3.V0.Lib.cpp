fdc311e4f205638eec6ec859023e77ac9e2ddd9b
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-09-07 16:53:58 +0500

    Task3

---
+++ Tyuiu.NosovaVD.Sprint0.Task3.V0.Lib/Tyuiu.NosovaVD.Sprint0.Task3.V0.Lib.cpp
@@ -0,0 +1,25 @@
+﻿// Tyuiu.NosovaVD.Sprint0.Task3.V0.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: Это пример библиотечной функции.
+class ISprint0Task3V0 
+{
+public:
+	virtual int Add(int a, int b, int c) = 0; 
+	virtual int Subraction(int a, int b, int c) = 0;
+};
+class Service1  : public ISprint0Task3V0
+{
+	virtual int Add(int a, int b, int c) override  
+	{
+		return a + b + c;
+	}
+	virtual int Subraction(int a, int b, int c) override
+	{
+		return a - b - c;
+	}
+};
---
+++ Tyuiu.NosovaVD.Sprint0.Task3.V0.Test/Tyuiu.NosovaVD.Sprint0.Task3.V0.Test.cpp
@@ -0,0 +1,50 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.NosovaVD.Sprint0.Task3.V0.Lib/Tyuiu.NosovaVD.Sprint0.Task3.V0.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(TyuiuNosovaVDSprint0Task3V0Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task3V0* date = new Service1();
+			int a = 3;
+			int b = 4;
+			int c = 5;
+			int d = date->Add(a, b, c);
+			Assert::AreEqual(12, d);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			ISprint0Task3V0* date = new Service1();
+			int a = 10;
+			int b = 3;
+			int c = 8;
+			int d = date->Add(a, b, c);
+			Assert::AreEqual(21, d);
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			ISprint0Task3V0* date = new Service1();
+			int a = 25;
+			int b = 6;
+			int c = 9;
+			int d = date->Subraction(a, b, c);
+			Assert::AreEqual(10, d);
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			ISprint0Task3V0* date = new Service1();
+			int a = 40;
+			int b = 17;
+			int c = 4;
+			int d = date->Subraction(a, b, c);
+			Assert::AreEqual(19, d);
+		}
+	};
+}
