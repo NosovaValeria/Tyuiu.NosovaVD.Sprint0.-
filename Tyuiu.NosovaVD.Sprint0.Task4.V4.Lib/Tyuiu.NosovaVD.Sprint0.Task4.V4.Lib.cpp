3219144e4304ddf7817b923650f70a49a0201471
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-09-11 21:22:45 +0500

    Task4

---
+++ Tyuiu.NosovaVD.Sprint0.Task4.V4.Lib/Tyuiu.NosovaVD.Sprint0.Task4.V4.Lib.cpp
@@ -0,0 +1,16 @@
+﻿// Tyuiu.NosovaVD.Sprint0.Task4.V4.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: Это пример библиотечной функции.
+
+class Service4 : public ISprint0Task4
+{
+	virtual int Calculate(int a, int b, int c, int d) override
+	{
+		return a * b + c * d;
+	}
+};
---
+++ Tyuiu.NosovaVD.Sprint0.Task4.V4.Test/Tyuiu.NosovaVD.Sprint0.Task4.V4.Test.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.NosovaVD.Sprint0.Task4.V4.Lib/Tyuiu.NosovaVD.Sprint0.Task4.V4.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest3
+{
+	TEST_CLASS(UnitTest3)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task4* primer = new Service4();
+			int a = 2;
+			int b{ 3 };
+			int c = 7;
+			int d(4);
+			Assert::AreEqual(a * b + c * d, 34);
+		}
+	};
+}
