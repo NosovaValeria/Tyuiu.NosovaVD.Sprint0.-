856c637723d0a49717f74a5dadf01b39a47f5a55
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-09-12 18:11:13 +0500

    Task5

---
+++ Tyuiu.NosovaVD.Sprint0.Task5.V4.Lib/Tyuiu.NosovaVD.Sprint0.Task5.V4.Lib.cpp
@@ -0,0 +1,15 @@
+﻿// Tyuiu.NosovaVD.Sprint0.Task5.V4.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: Это пример библиотечной функции.
+class Service4 : public ISprint0Task5
+{
+	virtual int Zadacha(float a, float b, float c) override 
+	{
+		return ((2 * a) * b / 100) * c * 1000;
+	}
+};
---
+++ Tyuiu.NosovaVD.Sprint0.Task5.V4.Test/Tyuiu.NosovaVD.Sprint0.Task5.V4.Test.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.NosovaVD.Sprint0.Task5.V4.Lib/Tyuiu.NosovaVD.Sprint0.Task5.V4.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest4
+{
+	TEST_CLASS(UnitTest4)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task5* service4 = new Service4();
+			float a = 67;
+			float b = 8.5;
+			float c = 6.5;
+			float d = service4->Zadacha(a,b,c) / 1000.0;
+			float res = 74.035;
+			Assert::AreEqual(res, d);
+		}
+	};
+}
