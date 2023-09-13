a4fcbfe45ffebb76e27442ac5ce95f7e1c7e7ef0
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-09-13 09:13:18 +0500

    Task6

---
+++ Tyuiu.NosovaVD.Sprint0.Task6.V4.Lib/Tyuiu.NosovaVD.Sprint0.Task6.V4.Lib.cpp
@@ -0,0 +1,15 @@
+﻿// Tyuiu.NosovaVD.Sprint0.Task6.V4.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: Это пример библиотечной функции.
+class Service5 : public ISprint0Task6 
+{
+	virtual int Calculate(float x, int y) override 
+	{
+		return (5 * x) / (7 + y) * 1000.0;
+	}
+};
---
+++ Tyuiu.NosovaVD.Sprint0.Task6.V4.Test/Tyuiu.NosovaVD.Sprint0.Task6.V4.Test.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.NosovaVD.Sprint0.Task6.V4.Lib/Tyuiu.NosovaVD.Sprint0.Task6.V4.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest5
+{
+	TEST_CLASS(UnitTest5)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task6* service5 = new Service5();
+			float x = 3.5;
+			int y = 3;
+			float z = service5->Calculate(x, y)/1000.0;
+			float w = 1.75;
+			Assert::AreEqual(w, z);
+		}
+	};
+}
