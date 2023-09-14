17bfb2c52797da7f76fd0d3ce536ea61a13ae616
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-09-14 09:25:58 +0500

    Task7

---
+++ Tyuiu.NosovaVD.Sprint0.Task7.V4.Lib/Tyuiu.NosovaVD.Sprint0.Task7.V4.Lib.cpp
@@ -0,0 +1,22 @@
+﻿// Tyuiu.NosovaVD.Sprint0.Task7.V4.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: Это пример библиотечной функции.
+class Service : public ISprint0Task7
+{
+	virtual bool Rezalt(int a, int b) override
+	{
+		int a1 = a / 100000;
+		int a2 = (a / 10000) % 10;
+		int a3 = (a / 1000) % 10;
+		int a4 = (a / 100) % 10;
+		int a5 = (a / 10) % 10;
+		int a6 = a % 10;
+		bool p = (a1 == b) || (a2 == b) || (a3 == b) || (a4 == b) || (a5 == b) || (a6 == b);
+		return p;
+	}
+};
---
+++ Tyuiu.NosovaVD.Sprint0.Task7.V4.Test/Tyuiu.NosovaVD.Sprint0.Task7.V4.Test.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.NosovaVD.Sprint0.Task7.V4.Lib/Tyuiu.NosovaVD.Sprint0.Task7.V4.Lib.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest7
+{
+	TEST_CLASS(UnitTest7)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task7* service7 = new Service();
+			int a = 235671;
+			int b = 5;
+			bool c = service7->Rezalt(a, b);
+			Assert::AreEqual(true, c);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			ISprint0Task7* service7 = new Service();
+			int a = 235671;
+			int b = 8;
+			bool c = service7->Rezalt(a, b);
+			Assert::AreEqual(false, c);
+		}
+	};
+}
